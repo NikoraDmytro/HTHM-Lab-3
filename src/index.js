7aeaa474f7c9b3f62933417bf1e0b300fd60d3ea
Dmytro <dimaniko04@ukr.net>
2022-04-10 18:07:11 +0300

    Installed webpack devServer tool and a plugin for auto index.html compiling in dist folder. Created 'public' folder with index.html in it. App entry point was renamed to app.js. Created placeholder function for rendering list items.

--- src/index.js
+++
@@ -1,5 +0,0 @@
-function HelloWorld() {
-  document.body.insertAdjacentHTML("beforebegin", "<h1>Hello World</h1>");
-}
-
-HelloWorld();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
+    clean: true,
   },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 4000,
+    compress: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
