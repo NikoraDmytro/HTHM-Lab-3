f5c37fbedf8fd5ebdd0a67c5eb637705508ae9cd
Dmytro <igorNikora456@gmail.com>
2022-04-14 23:04:47 +0300

    Added third task page

--- src/pages/block1/task3/index.ts
+++ src/pages/block1/task3/index.ts
@@ -1,6 +1,20 @@
 import { renderPageWrapper } from "components/PageWrapper";
 
+import styles from "./styles.module.scss";
+
 const task2 = () => {
+  const main = document.getElementsByTagName("main")[0];
+  const button = main.getElementsByTagName("button")[0];
+  const paragraphs = main.getElementsByTagName("p");
+
+  button.className = styles.changeFontSizeBtn;
+
+  button.addEventListener("click", () => {
+    for (let paragraph of paragraphs) {
+      paragraph.setAttribute("style", "font-size: 15px");
+    }
+  });
+
   renderPageWrapper();
 };
 
