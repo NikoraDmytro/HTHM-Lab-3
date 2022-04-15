94f2d323fadd4549a87866329269abc54990d1ad
Dmytro <dimaniko04@ukr.net>
2022-04-15 11:38:05 +0300

    Fifth task page added.

--- src/pages/block1/task5/index.ts
+++ src/pages/block1/task5/index.ts
@@ -2,6 +2,20 @@ import { renderPageWrapper } from "components/PageWrapper";
 
 const task2 = () => {
   renderPageWrapper();
+
+  const fadeOutText =
+    document.getElementById("fadeOut") ?? document.createElement("p");
+  let opacity = 1;
+
+  const interval = setInterval(() => {
+    opacity -= 0.05;
+
+    fadeOutText.setAttribute("style", `opacity: ${opacity}`);
+
+    if (opacity <= 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
 };
 
 task2();
