228ed13aa1af05aed3d3e302be50444706fb7ad0
Dmytro <igorNikora456@gmail.com>
2022-04-20 10:23:37 +0300

    Finished first task of third block.

--- src/pages/block3/task1/index.ts
+++ src/pages/block3/task1/index.ts
@@ -1,19 +1,36 @@
+import { renderPageWrapper } from "components/PageWrapper";
 import styles from "./styles.module.scss";
 
 const task1 = () => {
-  const ad = document.querySelector("#adBlock") as HTMLDivElement;
-  const closeAd = document.querySelector("#closeAd") as HTMLButtonElement;
-  const hideAdExpiry = window.localStorage.getItem("hideAdExpiry");
+  renderPageWrapper();
+  const ad = document.querySelector("#adBlock");
+  const closeAd = document.querySelector("#closeAd");
 
-  ad.classList.add(styles.adBlock);
-  closeAd.classList.add(styles.closeAdBtn);
+  ad?.classList.add(styles.adBlock);
+  closeAd?.classList.add(styles.closeAdBtn);
+
+  const newsShowAdTimeout = () => {
+    const expireTime = window.localStorage.getItem("expireTime");
+
+    setTimeout(
+      () => {
+        ad?.classList.add(styles.showAd);
+        window.localStorage.removeItem("expireTime");
+      },
+      expireTime ? +expireTime - Date.now() : 0
+    );
+  };
+
+  newsShowAdTimeout();
 
   closeAd?.addEventListener("click", () => {
-    ad.remove();
+    const now = new Date();
+    const expireTime = now.setDate(now.getDate() + 1);
 
-    setTimeout(() => {
-      document.body.append(ad);
-    }, 1000);
+    ad?.classList.remove(styles.showAd);
+    window.localStorage.setItem("expireTime", expireTime.toString());
+
+    newsShowAdTimeout();
   });
 };
 
